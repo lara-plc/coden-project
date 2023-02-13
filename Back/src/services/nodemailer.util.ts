6ff311c3a19c8feb4a9708409e5fc1924e12e4a3
Lara Ponciano <poncianolaralc@gmail.com>
2023-02-13 00:20:41 -0300

    SMTP calling adjust

--- Back/src/services/nodemailer.util.ts
+++ Back/src/services/nodemailer.util.ts
@@ -20,16 +20,16 @@ const sendEmail = async({name,email,phone,subject,description}: IContactRequest)
         port: 587,
         secure: false,
         auth: {
-            user: process.env.SMTP_USER,
-            pass: process.env.SMTP_PASS
+            user: 'poncianolaralc@outlook.com',
+            pass: 'Bluehill2018!'
         }
     })
 
     //Criação do método sendMail
     //O método fará o envio do email de acordo com os parâmetros passados
     await transporter.sendMail({
-        from: process.env.SMTP_USER,
-        to: process.env.CRM_EMAIL,
+        from: 'poncianolaralc@outlook.com',
+        to: 'poncianolaralc@gmail.com',
         subject: subject,
         html: message
     }).then(() => {
